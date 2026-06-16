e91023ad2865d7ccc5c7a3664769a069429c876b
Gunjan <gunjan517b@gmail.com>
2026-06-16 23:17:19 +0530

    Updated

---
+++ assignment1.py
@@ -0,0 +1,97 @@
+1. What is the output of the following code? 
+a = 5 
+b = 10 
+print(a < b < 20) 
+ANS: TRUE 
+
+2. Predict the output: 
+x = True 
+y = False 
+print(x + y * x) 
+ANS:  1
+
+3. What does this expression evaluate to? 
+print(4 ** 0 ** 2) 
+ANS:  1
+
+4. Find the result of this bitwise operation: 
+a = 12   
+b = 5    
+print(a ^ b) 
+ANS: 9
+
+5. Guess the output: 
+print((3 and 0) or (0 and 3))
+ANS: 0
+ 
+6. What's the output of this tricky comparison? 
+print(256 is 256) 
+print(257 is 257)
+ANS: True
+
+7. Evaluate this expression: 
+a = 7 
+print(~a + 1) 
+ANS: -7          # -(x+1)
+
+8. What will this print? 
+a = True 
+b = False 
+print((a or b) and not (a and b)) 
+ANS: TRUE
+
+9. What's the output? 
+print(10 > 5 == True)
+ANS: FALSE
+ 
+10. Evaluate this expression: 
+print(2 + 3 * 4 ** 2 // 8 % 3) 
+ANS: 2
+
+11. What does this expression evaluate to? 
+print(1 << 2 + 1) 
+ANS:  8
+
+12. Predict the output: 
+a = 3 
+b = 2 
+a *= b + 1 
+print(a)
+ANS: 9  
+
+13. Evaluate this chained comparison: 
+print(3 < 5 > 2 == 2) 
+ANS: TRUE
+
+14. Guess the result: 
+print(10 // 3 * 3 + 1 % 3) 
+ANS: 10
+
+15. What is the result of this? 
+x = 10 
+y = 20 
+print(x & y | x ^ y) 
+ANS: 30
+
+16. Trick with boolean and bitwise: 
+a = True 
+b = False 
+print(a & b or a ^ b) 
+ANS: TRUE    
+
+17. Evaluate this: 
+x = 8 
+print(x >> 1 << 2) 
+ANS: 16
+
+18. What does this produce? 
+print(5 + True * False + not False) 
+ANS:  print(5 + True * False + (not False)) => 6
+             
+19. Operator confusion: 
+print((not 0) * (False or 1)) 
+ANS: 1
+
+20. Mix of precedence and associativity: 
+print(4 + 3 * 2 ** 2 // 2 - 1)
+ANS: 9
